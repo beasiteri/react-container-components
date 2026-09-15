dfcf95c0f410422a86fefb3f8a69cf1c7a5dfc0c
beasiteri <siteribea@gmail.com>
2026-09-15 15:16:00 +0200

    Rename base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/react-container-component/',
+  base: '/react-container-components/',
   plugins: [react(), tailwindcss()],
 });
